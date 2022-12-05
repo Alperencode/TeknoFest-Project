373efe3fbdc5b129c1fc5e3ff0070e8a13dd924a
Alperen Aga <alperencode@gmail.com>
2022-12-06 01:18:33 +0300

    Add comments to abstracted functions

--- ISBN-Barcode-Reader/sources/sources.py
+++ ISBN-Barcode-Reader/sources/sources.py
@@ -1,16 +1,26 @@
 from pyzbar.pyzbar import decode
 from isbnlib import *
 
+# Global variables
 result_dictionary = {}
 last_book = ""
 
 def ParseISBN(isbn):
+    """
+    Parsing the ISBN number
+    If ISBN is valid, return the metadata
+    Else return False
+    """
     if not is_isbn10(isbn) and not is_isbn13(isbn):
         return False
     else:
         return meta(isbn)
 
 def ParseMeta(isbn_meta):
+    """
+    Parsing the metadata from the ISBN
+    If data is valid, update the result dictionary
+    """
     global last_book
     if isbn_meta["Title"] != last_book:
         print("Title:", isbn_meta["Title"])
@@ -20,6 +30,9 @@ def ParseMeta(isbn_meta):
         result_dictionary[key] = value
 
 def OutputTXT():
+    """
+    Output the result dictionary to a txt file
+    """
     if result_dictionary:
         with open('output.txt', 'w',encoding='utf-8') as f:
             for key, value in result_dictionary.items():
@@ -34,4 +47,5 @@ def OutputTXT():
         pass
 
 def GetResult():
+    """ Getter for the result dictionary """
     return result_dictionary
